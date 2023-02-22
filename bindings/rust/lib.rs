091cabd114139925030011502f5a29423e44fdab
Amaan Qureshi <amaanq12@gmail.com>
2023-02-21 23:59:36 -0500

    style: update rust bindings with README and better file docs

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -28,17 +28,16 @@ pub fn language() -> Language {
     unsafe { tree_sitter_gosum() }
 }
 
+/// The source of the Rust tree-sitter grammar description.
+pub const GRAMMAR: &str = include_str!("../../grammar.js");
+
+/// The syntax highlighting query for this language.
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
-
-// Uncomment these to include any queries that this grammar contains
-
-pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
@@ -47,6 +46,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading gosum language");
+            .expect("Error loading gosum grammar");
     }
 }
